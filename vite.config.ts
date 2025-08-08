4fc03126127109e10914bd09dba5e443bad6b337
Ol1mowski <olimarki123@wp.pl>
2025-08-08 15:12:04 +0200

    chore: update ci cd configuration  for improved build process

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,27 @@
 /// <reference types="vitest" />
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-  ],
+  plugins: [react()],
   test: {
     globals: true,
     environment: 'jsdom',
     setupFiles: ['./src/test/setup.ts'],
     css: true,
+    // Dodajemy konfigurację dla CI/CD
+    pool: 'forks',
+    poolOptions: {
+      forks: {
+        singleFork: true,
+      },
+    },
   },
-})
+  // Konfiguracja dla build w CI/CD
+  build: {
+    rollupOptions: {
+      external: ['@rollup/rollup-linux-x64-gnu'],
+    },
+  },
+});
